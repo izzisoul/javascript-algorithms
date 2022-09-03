b7d1790c11da7f08e472bde0e359e1b8ea1c225f
Izabella Tuganova <izzisoul@yandex.ru>
2022-09-03 20:47:05 +0300

    4 задача

--- 05-find-vowels/script.js
+++ 05-find-vowels/script.js
@@ -9,6 +9,17 @@
  *
 */
 
+const vowels = ['а', 'я', 'о', 'ё', 'у', 'ю', 'ы', 'и', 'э', 'е'];
+function findVowels(str) {
+    const word = Array.from(str);
+    console.log(word);
+    for(let i = 0; i <= vowels.length - 1; i++) {
+        console.log(word.includes(vowels[i]));
+    }
+    
+}
+findVowels('здравствуй');
+
 function findVowels(str) {
     // Напишите код здесь
 }
