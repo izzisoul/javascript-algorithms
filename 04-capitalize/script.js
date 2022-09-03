e15dff653d0204b55acc8165076fc5944d129a0a
Izabella Tuganova <izzisoul@yandex.ru>
2022-09-03 14:06:59 +0300

    4 задача

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -8,23 +8,11 @@
 
 function capitalize(str) {
     const strToArr = str.split(` `);
-    //console.log(strToArr);
     for(let i = 0; i <= strToArr.length - 1; i++) {
-        const capLetter = strToArr[i][0].toUpperCase() + strToArr[i].slice(1);
-        return capLetter;
+        const capLetter = [strToArr[i][0].toUpperCase() + strToArr[i].slice(1)];
         console.log(capLetter);
-        //const wholeWord = capLetter.split(` `);
-       // console.log(wholeWord);
-       // const together = wholeWord.reduce(function (previousValue, item){
-       //     return previousValue + item;
-       // });
-        //console.log(together);
-        //const sum = capLetter.reduce(function(previousValue, item){
-        //    return previousValue + item;
-        //});
-       // return capLetter.join(``);
-        }
-    }
+        }};
+    
     
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
